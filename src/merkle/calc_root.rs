0d979330b54d24f0a4db7140da0b49fca96ee135
Andrew Cooksey <andrewcooksey42@gmail.com>
2023-09-08 20:18:05 -0500

    small change

--- src/merkle/calc_root.rs
+++ src/merkle/calc_root.rs
@@ -19,7 +19,7 @@
 //! Drawing it out would look like this (The "" represents the empty strings we padded the base layer with):
 //!                                     root
 //!                     /                                \                   
-//!                   hash                              hash
+//!                   hash                               hash
 //!            /                  \                 /            \
 //!         hash                 hash             hash           hash
 //!      /        \          /        \         /      \       /      \         
@@ -30,8 +30,8 @@
 //! Notice each hash is a concatenation of the hashes of the blocks below it. 
 //! 
 
-use std::hash;
-
+use std::{hash::{self, Hasher}, collections::hash_map::DefaultHasher};
+use crate::merkle::calc_root::hash::Hash;
 
 
 /// A hash value is a 64 bit unsigned integer.
@@ -49,23 +49,64 @@ pub fn pad_base_layer(blocks: &mut Vec<&str>) {
     }
 }
 
+/// The hashing algorithm is not specified, so we will use a simple DefaultHasher.
+/// Let this be algorithm for hashing. We will call this function with our data to hash it.
+/// Notice that we are using a trait bound here. This is so we can hash any type that implements the Hash trait.
+pub fn hash<T: Hash>(t: &T) -> HashValue {
+    let mut s = DefaultHasher::new();
+    t.hash(&mut s);
+    s.finish()
+}
+
 /// This is where we concatenate the hash values.
-/// The hashing algorithm is not specified, so we will use a simple SHA256.
-pub fn concatenate_hash_values(mut left: HashValue, mut right: HashValue) -> HashValue {
-    todo!()
+/// There are better ways to do this, but the purpose of this is to demonstrate the concept.
+pub fn concatenate_hash_values(left: HashValue, right: HashValue) -> HashValue {
+    let left = left.to_le_bytes();
+    let right = right.to_le_bytes();
+
+    let mut cmb: Vec<u8> = Vec::with_capacity(left.len() + right.len());
+    cmb.extend_from_slice(&left);
+    cmb.extend_from_slice(&right);
+    hash(&cmb)
 }
 
 
-pub fn calc_root() {
-    todo!()
-}
+pub fn calc_root(sentence: &str) -> HashValue {
+    let mut leafs = sentence.split_whitespace().collect::<Vec<&str>>();
+    pad_base_layer(&mut leafs);
 
+    let mut queue = leafs
+        .iter()
+        .rev()
+        .map(|x| hash(x))
+        .collect::<Vec<HashValue>>();
+
+    while queue.len() > 1 {
+        let pair: (u64, u64) = (queue.pop().unwrap(), queue.pop().unwrap());
+        let hash: u64 = concatenate_hash_values(pair.0, pair.1);
+        queue.insert(0, hash);
+    }
+
+    queue[0]
+}
 
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn my_test() {
-        // ...
+    fn calc_root_test() {
+        let data = "My name is Jeff";
+        let root = calc_root(data);
+
+        
+
+        for word in data.split(" ").into_iter() {
+            println!("word:    {:?}", word);
+
+        }
+
+
     }
 }
